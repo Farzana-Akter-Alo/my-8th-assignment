e744ad7a1441f8d2472bf2b3c5df3e366dbebd56
Farzana Akter Alo <mstfarzana161@gmail.com>
2026-02-04 00:06:14 +0600

    added some changes

--- src/Component/Route/Route.jsx
+++ src/Component/Route/Route.jsx
@@ -2,7 +2,6 @@ import App from "../../App";
 import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
 import Home from "../Home/Home";
-import About from "../About/About";
 import Install from "../Install/Install";
 import CardDetails from "../Pages/CardDetails";
 import SingleCardDetails from "../Pages/SingleCardDetails";
@@ -11,29 +10,26 @@ export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
+    errorElement: <Error />,
     children: [
       {
         index: true,
         loader: () => fetch("./data.json"),
-        Component: Home,
+        element: <Home />,
       },
       {
         path: "app",
         loader: () => fetch("./data.json"),
-        Component: CardDetails,
+        element: <CardDetails />,
       },
       {
         path: "app/:id",
         loader: () => fetch("./data.json"),
-        Component: SingleCardDetails,
+        element: <SingleCardDetails />,
       },
       {
         path: "installization",
-        Component: Install,
-      },
-      {
-        path: "*",
-        Component: Error,
+        element: <Install />,
       },
     ],
   },
